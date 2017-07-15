867f84ec6fdfb725236be44a8ebdc026e8725b17
Maxim <maxtoyberman@gmail.com>
2017-07-15 17:13:23 +0300

    Change Drawer styles

--- App/Containers/Styles/DrawerContentStyles.js
+++ App/Containers/Styles/DrawerContentStyles.js
@@ -1,7 +1,45 @@
+import {Colors} from '../../Themes/'
+
 export default {
   container: {
+    flex: 1
+  },
+  actionImage: {
+    width: 20,
+    height: 20,
+    marginRight:10
+  },
+  actionRow: {
+    flexDirection: 'row',
+    marginBottom:10,
+    marginTop:10,
+  },
+  actionText: {
+    color: Colors.snow
+  },
+  avatar: {
+    alignSelf:'center',
+    left:15
+  },
+  divider: {
+    borderBottomColor: Colors.snow,
+    borderBottomWidth: 0.25
+  },
+  userName: {
     flex: 1,
-    padding: 20
+    alignSelf:'center',
+    left: 25,
+    color: Colors.snow
+  },
+  drawerHeader: {
+    flex: 1.3,
+    flexDirection: 'row',
+    backgroundColor: Colors.darkOrange,
+  },
+  drawerFooter: {
+    flex: 7,
+    backgroundColor: Colors.navBarOrange,
+    marginLeft:50,    
   },
   logo: {
     alignSelf: 'center'
--- App/Navigation/Styles/NavigationContainerStyles.js
+++ App/Navigation/Styles/NavigationContainerStyles.js
@@ -5,7 +5,7 @@ export default {
     flex: 1
   },
   navBar: {
-    backgroundColor: Colors.background
+    backgroundColor: Colors.navBarOrange
   },
   title: {
     color: Colors.snow
